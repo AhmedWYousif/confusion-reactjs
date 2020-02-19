80bc57d53720fef7e561a32a8d01d481c4cf0972
Ahmed Yousif <ahmed.yousif@intigral.net>
2020-02-19 23:42:42 +0400

    adding aboutus component

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -5,6 +5,7 @@ import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
+import AboutUs from './AboutComponent';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
@@ -21,7 +22,7 @@ class Main extends Component {
             comments: COMMENTS,
             promotions: PROMOTIONS,
             leaders: LEADERS
-          };
+        };
     }
 
     onDishSelect(dishId) {
@@ -33,21 +34,21 @@ class Main extends Component {
     render() {
 
         const HomePage = () => {
-            return(
-                <Home 
+            return (
+                <Home
                     dish={this.state.dishes.filter((dish) => dish.featured)[0]}
                     promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
                     leader={this.state.leaders.filter((leader) => leader.featured)[0]}
                 />
             );
-          };
+        };
 
-          const DishWithId = ({match}) => {
-            return(
-                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
-                  comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+        const DishWithId = ({ match }) => {
+            return (
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
             );
-          };
+        };
 
         return (
             <div>
@@ -55,13 +56,11 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
-                    <Route exact path='/contactus' component={Contact}  />
+                    <Route exact path='/contactus' component={Contact} />
+                    <Route exact path='/aboutus' component={() => <AboutUs leaders={this.state.leaders} />} />
                     <Route path='/menu/:dishId' component={DishWithId} />
                     <Redirect to="/home" />
                 </Switch>
-
-                {/* <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} /> */}
                 <Footer />
             </div>
         );
